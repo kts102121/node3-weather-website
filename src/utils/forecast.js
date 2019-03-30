7f7ebe583415537e860ffa1881e67b99997cfc78
kts102121 <ron@vrexlab.com>
2019-03-30 19:19:45 +0900

    fix) Report high and low temperature too

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,6 +1,14 @@
 const request = require('request');
 require('dotenv').config();
 
+//
+// Goal: Add new data to forecast
+//
+// 1. Update the forecast string to include new data
+// 2. Commit your changes
+// 3. Push your changes to GitHub and deploy to Heroku
+// 4. Test your work in the live application
+
 const forecast = (lat, lng, callback) => {
 
     const url = 'https://api.darksky.net/forecast/'+process.env.DARKSKY_APIKEY+'/'+ lat + ',' + lng + '?units=si&lang=ko';
@@ -11,8 +19,9 @@ const forecast = (lat, lng, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined);
         } else {
+            console.log(body.daily.data[0]);
             callback(undefined,
-                body.daily.data[0].summary + '. It is currently '+body.currently.temperature+' degrees out. There is a '+body.currently.precipProbability+'% of rain.');
+                body.daily.data[0].summary + '. It is currently '+body.currently.temperature+' degrees out. The high today is ' + body.daily.data[0].temperatureHigh +' with a low is ' + body.daily.data[0].temperatureLow + '. There is a '+body.currently.precipProbability+'% of rain.');
         }
     });
 };
